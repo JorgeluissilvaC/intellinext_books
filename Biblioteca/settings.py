bc269630c2ed56fc77ed9ca9be3c88d2f4117628
Jorgeluissilvac <jlsilva@uninorte.edu.co>
2020-05-19 21:06:17 -0500

    Lista de requerimientos

--- Biblioteca/settings.py
+++ Biblioteca/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c98ttc1kh^uel8%mp4dw9dh2vj$r#v&ftjx3m6+eo4s&7wb_r6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
